7ca8c56d4a24bce1bd430af0951f2dfa6da650ff
LunarIGuess <tony567020@gmail.com>
2024-09-14 21:29:05 -0700

    Art for background might change later and knife waiting on enemy stoof

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -5,7 +5,7 @@ const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var Hdirection := Input.get_axis("Left", "Right")
