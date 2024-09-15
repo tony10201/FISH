22f09f09902c20be2f8b73cf38aea11bd1a9df1a
LunarIGuess <tony567020@gmail.com>
2024-09-15 15:41:26 -0700

    Added knife swapping direction and animation yippieee

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
-
+var movingLeft = true
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -11,16 +11,23 @@ func _physics_process(_delta: float) -> void:
 	var Hdirection := Input.get_axis("Left", "Right")
 	if Hdirection:
 		velocity.x = Hdirection * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if velocity.x >= .5:
 		$Sprite2D.flip_h = true
+		movingLeft = false
+		$Knife.global_position.x = global_position.x + 45
+		$Knife.global_rotation = global_rotation + 45
 	if velocity.x <= -0.5:
 		$Sprite2D.flip_h = false
+		movingLeft = true
+		$Knife.global_position.x = global_position.x - 45
+		$Knife.global_rotation = global_rotation
 	var Vdirection := Input.get_axis("Up", "Down")
 	if Vdirection:
 		velocity.y = Vdirection * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
 	move_and_slide()
+	
--- Assets/Player/knife.gd
+++ Assets/Player/knife.gd
@@ -1,7 +1,20 @@
-extends Area2D
-
+extends Node2D
+@onready var animation_player = $Knife/AnimationPlayer
+@onready var Player = find_parent("Player")
 var damage: int = 1
 var speed: float = 1.0
+
+func _process(delta: float) -> void:
+	animationStuff()
+	
+
+func animationStuff():
+	if not animation_player.is_playing():
+		animation_player.stop()
+	if Input.is_action_just_pressed("LeftClick") and Player.movingLeft == true:
+		animation_player.play("SwingLeft")
+	if Input.is_action_pressed("LeftClick") and Player.movingLeft == false:
+		animation_player.play("SwingRight")
 
 func detectHit(body) -> void:
 	if body.is_in_group("Enemy"):
