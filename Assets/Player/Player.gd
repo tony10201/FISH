f0ae00ddf065cc74006c3e531e5bc79e817b46a3
LunarIGuess <tony567020@gmail.com>
2024-09-14 20:55:53 -0700

    enemy stuff denelix can figure out

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -1,25 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
+	var Hdirection := Input.get_axis("Left", "Right")
+	if Hdirection:
+		velocity.x = Hdirection * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.x >= .5:
+		$Sprite2D.flip_h = true
+	if velocity.x <= -0.5:
+		$Sprite2D.flip_h = false
+	var Vdirection := Input.get_axis("Up", "Down")
+	if Vdirection:
+		velocity.y = Vdirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
