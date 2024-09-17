ae4d1b5a40718f48d43c23b6096f24822486af5c
LunarIGuess <tony567020@gmail.com>
2024-09-17 10:21:37 -0700

    Enemies die on left click :3 now i need more to spawn and stoof :O

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -7,6 +7,22 @@ var movingLeft = true
 var health:int = 10
 
 func _physics_process(_delta: float) -> void:
+	inputs()
+	#animation stoof idk flip if moving at a certain way
+	if velocity.x >= .5:
+		$Sprite2D.flip_h = true
+		movingLeft = false
+		$Knife.global_position.x = global_position.x + 45
+		$Knife.global_rotation = global_rotation + 45
+	if velocity.x <= -0.5:
+		$Sprite2D.flip_h = false
+		movingLeft = true
+		$Knife.global_position.x = global_position.x - 45
+		$Knife.global_rotation = global_rotation
+	move_and_slide()
+	
+
+func inputs():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var Hdirection := Input.get_axis("Left", "Right")
@@ -21,16 +37,3 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = Vdirection * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-	#animation stoof idk flip if moving at a certain way
-	if velocity.x >= .5:
-		$Sprite2D.flip_h = true
-		movingLeft = false
-		$Knife.global_position.x = global_position.x + 45
-		$Knife.global_rotation = global_rotation + 45
-	if velocity.x <= -0.5:
-		$Sprite2D.flip_h = false
-		movingLeft = true
-		$Knife.global_position.x = global_position.x - 45
-		$Knife.global_rotation = global_rotation
-	move_and_slide()
-	
--- Assets/Player/knife.gd
+++ Assets/Player/knife.gd
@@ -2,11 +2,14 @@ extends Node2D
 @onready var animation_player = $Knife/AnimationPlayer
 @onready var Player = find_parent("Player")
 var damage: int = 1
-var speed: float = 1.0
+var attackSpeed: float = 1.0
+var enemies:Array = []
+
 
 #its the update function watcha expect _delta cause im not using it :3.
 func _process(_delta: float) -> void:
 	animationStuff()
+	attack()
 	
 #animation changes depending on direction
 func animationStuff():
@@ -16,8 +19,20 @@ func animationStuff():
 		animation_player.play("SwingLeft")
 	if Input.is_action_pressed("LeftClick") and Player.movingLeft == false:
 		animation_player.play("SwingRight")
-#Checks parent group and deletes if true.
-func detectHit(body:StaticBody2D) -> void:
+
+func attack():
+	if Input.is_action_just_pressed("LeftClick"):
+		if enemies.size() > 0:
+			var enemy
+			for element in enemies:
+				enemy = element
+				enemy.queue_free()
+
+#Adds enemy body into group only enemys should be allowed
+#so no way a random node gets in right? -Lunar
+func detectHit(body) -> void:
 	if body.is_in_group("Enemy"):
-		print("Enemy died!")
-		body.get_parent().queue_free()
+		enemies.append(body.get_parent())
+#Checks if the body has left removes from array maybe could be better lol -Lunar
+func detectExit(body) -> void:
+	enemies.erase(body.get_parent())
